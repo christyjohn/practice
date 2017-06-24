b5ddc140efaff1a8754fe4540f05626c684b86a6
Christy John <christyjohn.crz@gmail.com>
2017-06-24 17:54:26 +0530

    Bineary Search

--- datastructures/arrays/BinarySearch.java
+++ datastructures/arrays/BinarySearch.java
@@ -1,4 +1,33 @@
 public class BinarySearch 
 {
-	
+	public static void main(String[] args)
+	{
+		int[] numbers = {23, 34, 67, 78, 79, 80, 92, 95, 97, 100};
+		int key = 34;
+
+		binSearch(numbers,key);
+	}	
+
+	static void binSearch(int[] arr, int key)
+	{
+		int low = 0;
+		int high = arr.length - 1;
+		int mid;
+
+		while(low <= high) 
+		{
+			mid = low + high / 2;
+
+			if(key == arr[mid]){
+				System.out.println("The key found at position " + mid);
+				break;
+			}
+			else if(key > arr[mid])
+				low = mid + 1;
+			else if(key < arr[mid])
+				high = mid - 1;
+			else
+				System.out.println("The key is not present in the array.");
+		}
+	}
 }
