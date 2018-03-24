9e6a2614274b97eb739fafc4c9d010dbf0857358
Christy John <christyjohn.crz@gmail.com>
2018-03-24 17:10:51 +0530

    Update BubbleSort.java
    Updating Bubble Sort

--- algorithms/sorting/BubbleSort.java
+++ algorithms/sorting/BubbleSort.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class BubbleSort
 {
+	private static List<Integer> randArray = new ArrayList();
 	public static void main(String[] args)
 	{
-		List<Integer> randArray = new ArrayList();
 		System.out.println("Enter the numbers to be sorted\n" +
 			"Enter Q to finish entering.");
 		Scanner scanner = new Scanner(System.in);
@@ -18,7 +18,28 @@ public class BubbleSort
 			token = scanner.nextLine();
 		}
 
-		// to do 
-		// implement the rest
+		// Implementing bubble sort
+		int in, out;
+		
+		for(out = randArray.size() - 1; out > 1; out--) {
+			for(in = 0; in < out; in++) {
+				if(randArray.get(in) > randArray.get(in +1)) {
+					swap(in, in+1);
+				}
+			}
+		}
+	
+		// Printing the sorted list.
+		for(int i : randArray)
+		{
+			System.out.print(i + " ");
+		}
+		System.out.println("");
+	}
+		
+	public static void swap(int one, int two) {
+		int temp = randArray.get(one);
+		randArray.set(one, randArray.get(two));
+		randArray.set(two, temp);
 	}
 }
