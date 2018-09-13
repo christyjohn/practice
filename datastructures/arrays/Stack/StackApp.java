8e458e7ec64279acd32a8ee31e0c1bfbab92f3ed
Christy John <christyjohn.crz@gmail.com>
2018-09-13 12:27:46 +0530

    Update StackApp.java
    Separated out the StackX class to another class.

--- datastructures/arrays/Stack/StackApp.java
+++ datastructures/arrays/Stack/StackApp.java
@@ -1,49 +1,12 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class StackX {
-	private int maxSize;
-	private int top ;
-	private int[] stackArray;
-	
-	public StackX(int size) {
-		stackArray = new int[size];
-		maxSize = size;
-		top = -1;
-	}
-	
-	public void push(int val) {
-		stackArray[++top] = val;
-	}
-	
-	public int pop() {
-		int toReturn = stackArray[top];
-		stackArray[top--] = 0; // not in real case, just to demonstrate
-		return toReturn;
-	}
-	
-	public int peek() {
-		return stackArray[top];
-	}
-	
-	public boolean isEmpty() {
-		if(stackArray[top] == -1)
-			return true;
-		else
-			return false;
-	}
-	
-	public boolean isFull() {
-		if(top ==  maxSize-1)
-			return true;
-		else
-			return false;
-	}
-	
-	public void printStack() {
-		System.out.println(Arrays.toString(stackArray));
-	}
-}
+/*The base stack class is in separated out as a separate class as 
+ * it is being used in multiple programs.
+ *
+ * It is present in the same package at 
+ * https://github.com/christyjohn/practice/blob/master/datastructures/arrays/Stack/StackX.java
+ */
 
 public class StackApp {
 	public static void main(String[] args) {
