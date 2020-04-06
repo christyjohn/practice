95fb3a9125603ff1e8355c35fedf5966414fc413
Christy John <christyjohn.crz@gmail.com>
2020-04-06 14:47:08 +0530

    shadowing variables

---
+++ c++/learncpp/ch06/shadow_localvar2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    int apples { 5 };
+
+    {
+        cout << apples << '\n';
+
+        //int apples { 0 };
+
+        apples = 10;
+
+        cout << apples << '\n';
+    }
+
+    cout << apples << '\n';
+
+    return 0;
+}
