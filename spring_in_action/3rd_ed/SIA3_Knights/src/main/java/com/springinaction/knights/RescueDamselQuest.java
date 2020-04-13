fd0fbe5037bb885a16471c86f45ce5a531091d4a
Christy John <christyjohn.crz@gmail.com>
2020-04-13 13:43:04 +0530

    rescue damsel quest

---
+++ spring_in_action/3rd_ed/SIA3_Knights/src/main/java/com/springinaction/knights/RescueDamselQuest.java
@@ -0,0 +1,8 @@
+package com.springinaction.knights;
+
+public class RescueDamselQuest implements Quest {
+	public void embark() throws QuestException
+	{
+		System.out.println("Rescuing damsel in distress");
+	}
+}
