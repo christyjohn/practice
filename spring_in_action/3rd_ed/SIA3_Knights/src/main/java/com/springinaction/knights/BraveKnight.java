e6772762fe3369c43e933941f75b685fd85b3440
Christy John <christyjohn.crz@gmail.com>
2020-04-13 12:13:56 +0530

    Bravenight example and mockito test

---
+++ spring_in_action/3rd_ed/SIA3_Knights/src/main/java/com/springinaction/knights/BraveKnight.java
@@ -0,0 +1,13 @@
+package com.springinaction.knights;
+
+public class BraveKnight {
+	private Quest quest;
+	
+	public BraveKnight(Quest quest) {
+		this.quest = quest;
+	}
+	
+	public void embarkOnQuest() throws QuestException {
+		quest.embark();
+	}
+}
