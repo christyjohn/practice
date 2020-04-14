018b020d7b4c7900d8c6db3e67c63fb3dc18e04b
Christy John <christyjohn.crz@gmail.com>
2020-04-14 17:34:03 +0530

    springidol aop - passing parameters to aspects

--- spring_in_action/3rd_ed/SIA3_SpringIdol_AOP/src/main/java/com/springinaction/springidol/SpringIdolMain.java
+++ spring_in_action/3rd_ed/SIA3_SpringIdol_AOP/src/main/java/com/springinaction/springidol/SpringIdolMain.java
@@ -6,8 +6,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringIdolMain {
 
 	public static void main(String[] args) throws PerformanceException {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+		ApplicationContext ctx;
+		/*
+		ctx = new ClassPathXmlApplicationContext(
 				"com/springinaction/springidol/spring-idol.xml");
+		
+		ctx = new ClassPathXmlApplicationContext(
+				"com/springinaction/springidol/spring-idol2.xml");
+		*/
+		ctx = new ClassPathXmlApplicationContext(
+				"com/springinaction/springidol/spring-idol-around.xml");
+		
 		Performer performer = (Performer)ctx.getBean("eddie");
 		performer.perform();
 	}
