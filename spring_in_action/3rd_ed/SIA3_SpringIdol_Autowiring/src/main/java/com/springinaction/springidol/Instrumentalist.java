c642d83382b84076a8f05b67114e5e3cd144543b
Christy John <christyjohn.crz@gmail.com>
2020-04-14 09:48:21 +0530

    springidol autowiring - byName

---
+++ spring_in_action/3rd_ed/SIA3_SpringIdol_Autowiring/src/main/java/com/springinaction/springidol/Instrumentalist.java
@@ -0,0 +1,35 @@
+package com.springinaction.springidol;
+
+public class Instrumentalist implements Performer {
+	private String song;
+	private Instrument instrument;
+
+	public Instrumentalist() {
+	}
+
+	public void perform() throws PerformanceException {
+		System.out.print("Playing " + song + " : ");
+		instrument.play();
+	}
+
+	public Instrument getInstrument() {
+		return instrument;
+	}
+
+	public void setInstrument(Instrument instrument) {
+		this.instrument = instrument;
+	}
+
+	public String getSong() {
+		return song;
+	}
+
+	public void setSong(String song) {
+		this.song = song;
+	}
+
+	public String screamSong() {
+		return song;
+	}
+
+}
