4aae98953bb2e992517ceaca41b1a25cd0781848
Christy John <christyjohn.crz@gmail.com>
2020-04-18 15:00:43 +0530

    udemy c++ course

--- angular/udemy_max_schwarmuller/my-first-app/src/app/server/server.component.ts
+++ angular/udemy_max_schwarmuller/my-first-app/src/app/server/server.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
    selector: 'app-server',
-   //selector: '[app-server]',
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
---
+++ c++/udemy_Abdul_Bari/sec05_cpp_basics/welcome.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+using std::string;
+
+int main()
+{
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello " << name << endl;
+
+    return 0;
+}
