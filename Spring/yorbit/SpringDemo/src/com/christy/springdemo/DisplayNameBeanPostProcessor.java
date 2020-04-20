a90e5096296272fe47f92ad164dd7239963be162
Christy John <christyjohn.crz@gmail.com>
2020-04-20 17:13:25 +0530

    Lifecycle Callbacks - DisplayNameBeanPostProcessor

---
+++ Spring/yorbit/SpringDemo/src/com/christy/springdemo/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,23 @@
+package com.christy.springdemo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("In After initialization method: Bean name is " +
+				beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("In Before initialization method: Bean name is " +
+				beanName);
+		return bean;
+	}
+
+	
+}
