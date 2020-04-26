54b9b3e36f124a682f64114739fa1d74b9a7cf25
Christy John <christyjohn.crz@gmail.com>
2020-04-26 16:31:52 +0530

    matrix calculations

---
+++ c++/udemy_Abdul_Bari/sec_08_arrays/add_matrices.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int A[2][3] = { {1, 2, 3}, {4, 5, 6} };
+    int B[2][3] = { {7, 8, 9}, {10, 11, 12} };
+
+    int C[2][3];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    //print C
+     for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << C[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+
+    return 0;
+}
