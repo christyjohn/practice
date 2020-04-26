9c9678294f7a2098fcbc89e9c7cfd7b2b953058a
Christy John <christyjohn.crz@gmail.com>
2020-04-26 15:22:38 +0530

    creating triangle patterns

---
+++ c++/udemy_Abdul_Bari/sec_08_arrays/bottom_right_triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 3; j >= 0; j--)
+        {
+            if (j <= i)
+                cout << "*" << " ";
+            else
+                cout << "  ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
