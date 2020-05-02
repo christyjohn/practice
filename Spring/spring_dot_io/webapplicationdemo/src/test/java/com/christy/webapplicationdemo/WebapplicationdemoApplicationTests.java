3cda106aa61e7e1bab714c09026c9e45245b443c
Christy John <christyjohn.crz@gmail.com>
2020-05-02 22:03:46 +0530

    Spring Boot web application

---
+++ Spring/spring_dot_io/webapplicationdemo/src/test/java/com/christy/webapplicationdemo/WebapplicationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christy.webapplicationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapplicationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
