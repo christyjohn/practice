2f7a5150296f6d96666c04eeed31043bc798718f
Christy John <christyjohn.crz@gmail.com>
2020-05-24 00:56:48 +0530

    Writing a JAX-RS client Best practices

--- Java/webservices/javabrains/REST/advanced-jaxrs-06/src/main/java/com/christy/rest/client/RestApiClient.java
+++ Java/webservices/javabrains/REST/advanced-jaxrs-06/src/main/java/com/christy/rest/client/RestApiClient.java
@@ -2,9 +2,8 @@ package com.christy.rest.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 import com.christy.messenger.model.Message;
 
@@ -20,10 +19,26 @@ public class RestApiClient {
 		Message message = response.readEntity(Message.class);
 		*/
 		
+		WebTarget baseTarget = client.target("http://localhost:8080/advanced-jaxrs-06/webapi/");
+		WebTarget messagesTarget = baseTarget.path("messages");
+		WebTarget singleMessageTarget = messagesTarget.path("{messageId}");
+		
+		/*
 		Message message = client
 				.target("http://localhost:8080/advanced-jaxrs-06/webapi/messages/1")
 				.request()
 				.get(Message.class);
-		System.out.println(message.getMessage());
+		*/
+		Message message1 = singleMessageTarget
+				.resolveTemplate("messageId", "1")
+				.request(MediaType.APPLICATION_JSON)
+				.get(Message.class);
+		System.out.println(message1.getMessage());
+		
+		Message message2 = singleMessageTarget
+				.resolveTemplate("messageId", "2")
+				.request(MediaType.APPLICATION_JSON)
+				.get(Message.class);
+		System.out.println(message2.getMessage());
 	}
 }
