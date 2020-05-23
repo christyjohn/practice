7e1fbc9c00a98932434cf8f04d0ebd5633bfc156
Christy John <christyjohn.crz@gmail.com>
2020-05-23 12:59:51 +0530

    Completed Profile section

---
+++ Java/webservices/javabrains/REST/messenger/src/main/java/com/christy/messenger/service/ProfileService.java
@@ -0,0 +1,42 @@
+package com.christy.messenger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.christy.messenger.database.DatabaseClass;
+import com.christy.messenger.model.Profile;
+
+public class ProfileService {
+	private Map<String, Profile> profiles = DatabaseClass.getProfiles();
+	
+	public ProfileService() {
+		profiles.put("christy", new Profile(1L, "christy", "Christy", "John"));
+	}
+	
+	public List<Profile> getAllProfiles() {
+		return new ArrayList<Profile>(profiles.values());
+	}
+	
+	public Profile getProfile(String profileName) {
+		return profiles.get(profileName);
+	}
+	
+	public Profile addProfile(Profile profile) {
+		profile.setId(profiles.size() + 1);
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public Profile updateProfile(Profile profile) {
+		if (profile.getProfileName().isEmpty()) {
+			return null;
+		}
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public Profile removeProfile(String profileName) {
+		return profiles.remove(profileName);
+	}
+}
