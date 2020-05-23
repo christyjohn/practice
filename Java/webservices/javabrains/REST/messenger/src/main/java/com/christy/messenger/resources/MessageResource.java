66fe029dfd1cd9b02850ad38c312379e2638a490
Christy John <christyjohn.crz@gmail.com>
2020-05-23 20:44:51 +0530

    Content Negotiation

--- Java/webservices/javabrains/REST/messenger/src/main/java/com/christy/messenger/resources/MessageResource.java
+++ Java/webservices/javabrains/REST/messenger/src/main/java/com/christy/messenger/resources/MessageResource.java
@@ -23,7 +23,7 @@ import jakarta.ws.rs.core.UriInfo;
 
 @Path("/messages")
 @Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
+@Produces(value= { MediaType.APPLICATION_JSON, MediaType.TEXT_XML} )
 public class MessageResource {
 
 	MessageService messageService = new MessageService();
