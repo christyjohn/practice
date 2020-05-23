a60ee75cfc6efa7e258d1f4066c3395c44e39901
Christy John <christyjohn.crz@gmail.com>
2020-05-24 00:42:39 +0530

    Writing a JAX-RS client explained

--- Java/webservices/javabrains/REST/advanced-jaxrs-06/src/main/java/com/christy/rest/client/RestApiClient.java
+++ Java/webservices/javabrains/REST/advanced-jaxrs-06/src/main/java/com/christy/rest/client/RestApiClient.java
@@ -2,6 +2,8 @@ package com.christy.rest.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
 import com.christy.messenger.model.Message;
@@ -10,10 +12,18 @@ public class RestApiClient {
 	public static void main(String[] args) {
 		
 		Client client = ClientBuilder.newClient();
-		Response response = 
-				client.target("http://localhost:8080/advanced-jaxrs-06/webapi/messages/1")
-				.request().get();
+		/*
+		// what exactly is the code
+		WebTarget target = client.target("http://localhost:8080/advanced-jaxrs-06/webapi/messages/1");
+		Builder builder = target.request(MediaType.APPLICATION_JSON);
+		Response response = builder.get();
 		Message message = response.readEntity(Message.class);
+		*/
+		
+		Message message = client
+				.target("http://localhost:8080/advanced-jaxrs-06/webapi/messages/1")
+				.request()
+				.get(Message.class);
 		System.out.println(message.getMessage());
 	}
 }
