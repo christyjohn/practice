3549e206f6dd25f0269ccfbbf0364e915a743470
Christy John <christyjohn.crz@gmail.com>
2020-05-24 23:22:59 +0530

    Deleted some code to start fresh

--- c++/udemy_Frank_Mitropoulos/sec02_setup/MyWorkspace/Project2/main.cpp
+++
@@ -1,9 +0,0 @@
-#include  <iostream>
-using namespace std;
-
-int main()
-{
-    cout << "My Template" << endl;
-    
-    return 0;
-}
--- c++/udemy_Frank_Mitropoulos/sec04_getting_started/Project2/main.cpp
+++
@@ -1,8 +0,0 @@
-#include  <iostream>
-
-int main()
-{
-    std::cout << "Helllo Projct 2" << std::endl;
-    
-    return 0;
-}
