8a95f47c27bdc03740b99d55be49f1919b3209f8
Christy John <christyjohn.crz@gmail.com>
2020-07-03 12:42:20 +0530

    added a centered NetWorkImage widget

--- flutter-dart/udemy/dr_angela_yu/i_am_rich/lib/main.dart
+++ flutter-dart/udemy/dr_angela_yu/i_am_rich/lib/main.dart
@@ -3,9 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
-      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900],
+            centerTitle: true,
+          ),
+          backgroundColor: Colors.blueGrey,
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://www.w3schools.com/w3css/img_lights.jpg'),
+            ),
+          )),
     ),
   );
 }
