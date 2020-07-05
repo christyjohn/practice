c3aa08075a5d91188386d0a012d1c33ff8f607ae
Christy John <christyjohn.crz@gmail.com>
2020-07-05 16:09:57 +0530

    Layouts using rows and columns

--- flutter-dart/udemy/dr_angela_yu/mi_card_flutter_master/lib/main.dart
+++ flutter-dart/udemy/dr_angela_yu/mi_card_flutter_master/lib/main.dart
@@ -12,7 +12,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            verticalDirection: VerticalDirection.down,
+            //mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.blue,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.red,
+                child: Text('Container 3'),
+              ),
+              Container(
+                width: double.infinity,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
