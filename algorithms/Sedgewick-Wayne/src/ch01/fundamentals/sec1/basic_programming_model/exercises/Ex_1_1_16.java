6310a63c63154d571a1e21eae11c95c0b8c15888
Christy John <christyjohn.crz@gmail.com>
2020-07-21 22:14:12 +0530

    Histogram - recursion

---
+++ algorithms/Sedgewick-Wayne/src/ch01/fundamentals/sec1/basic_programming_model/exercises/Ex_1_1_16.java
@@ -0,0 +1,16 @@
+package ch01.fundamentals.sec1.basic_programming_model.exercises;
+
+public class Ex_1_1_16 {
+
+	public static void main(String[] args) {
+		System.out.println(exR1(6));
+	}
+	
+	private static String exR1(int n)
+	{
+		if (n <= 0)
+			return "";
+		return exR1(n-3) + n + exR1(n-2) + n;
+	}
+
+}
