4a51f5a4efbc69efd6f216bfb42d35c4d90f3a55
Christy John <christyjohn.crz@gmail.com>
2020-07-22 10:14:05 +0530

    formatted printing of string int values and divisiion

---
+++ algorithms/Sedgewick-Wayne/src/ch01/fundamentals/sec1/basic_programming_model/exercises/Wx_1_1_21.java
@@ -0,0 +1,29 @@
+package ch01.fundamentals.sec1.basic_programming_model.exercises;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Wx_1_1_21 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter a name followed by two marks: ");
+		
+		String[] arr;
+		
+		while (StdIn.hasNextLine()) {
+			String line = StdIn.readLine();
+			arr = line.split(" ");
+			printValues(arr);
+		}
+	}
+
+	public static void printValues(String[] arr) {
+		StdOut.printf("%s\t", arr[0]);
+		StdOut.printf("%d\t", Integer.parseInt(arr[1]));
+		StdOut.printf("%d\t", Integer.parseInt(arr[2]));
+		StdOut.printf("%4.3f", 
+				Double.parseDouble(arr[1]) / Double.parseDouble(arr[2]));
+		StdOut.println();
+	}
+}
