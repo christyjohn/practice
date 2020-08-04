4b29d3bb45a2a652c9ceb0dc7d566b99623b0a89
Christy John <christyjohn.crz@gmail.com>
2020-08-04 21:29:51 +0530

    add header

---
+++ C/KnR/ch01_introduction/exercises/ex_1_3.c
@@ -0,0 +1,25 @@
+#include  <stdio.h>
+
+/** print Fahrenheit-Celsius table
+ *  for fahr = 0, 20, ..., 300 floating-point version */
+    
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      /* lower llimit of temperature table */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+
+    fahr = lower;
+    printf("Fahrenheit\tCelsius\n");
+    printf("-----------------------\n");
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("%10.0f %12.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }    
+    return 0;
+}
