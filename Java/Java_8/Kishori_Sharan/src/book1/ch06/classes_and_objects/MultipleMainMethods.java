6e046a7424b5c8296ce36205210e2f183f7e16b1
Christy John <M1033385@mindtree.com>
2020-08-18 09:25:50 +0530

    Multiple main methods

---
+++ Java/Java_8/Kishori_Sharan/src/book1/ch06/classes_and_objects/MultipleMainMethods.java
@@ -0,0 +1,16 @@
+package book1.ch06.classes_and_objects;
+
+public class MultipleMainMethods {
+	public static void main(String[] args) {
+		/* May be used as the application entry point */
+	}
+	
+	public static void main(String[] args, int a) {
+		/* Another main() method */
+	}
+	
+	public int main() {
+		/* Another main() method */
+		return 0;
+	}
+}
