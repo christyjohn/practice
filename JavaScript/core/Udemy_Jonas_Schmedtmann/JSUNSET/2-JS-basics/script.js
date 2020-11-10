bdd3aa54012f5985b18d6a1dbcda6e14b8cb50b1
Christy John <christyjohn.crz@gmail.com>
2020-11-11 00:19:01 +0530

    Variables and Data types

---
+++ JavaScript/core/Udemy_Jonas_Schmedtmann/JSUNSET/2-JS-basics/script.js
@@ -0,0 +1,24 @@
+//console.log('Hello World!!!');
+
+/*********************
+* Variables and data types
+*/
+
+var firstName = 'Christy';
+console.log(firstName);
+
+var lastName = 'John';
+var age = 38;
+
+var legalAge = true;
+console.log(legalAge);
+
+var job;
+console.log(job);
+
+job = "Programmer";
+console.log(job);
+
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'John and Mark';
