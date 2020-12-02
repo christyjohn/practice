cee0b1346956af3ff1e8651ccb420b3c07f4c86f
Christy John <christyjohn.crz@gmail.com>
2020-12-02 22:52:14 +0530

    Dice rolling example

---
+++ Algorithms/Sedgewick_Wayne/src/ch01/sec2/data_abstraction/Rolls.java
@@ -0,0 +1,23 @@
+package ch01.sec2.data_abstraction;
+
+import edu.princeton.cs.algs4.Counter;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Rolls {
+	public static void main(String[] args) {
+		int t = Integer.parseInt(args[0]);
+		int sides = 6;
+		Counter[] rolls = new Counter[sides+1];
+		for (int i = 1; i <= sides; i++)
+			rolls[i] = new Counter(i + "'s");
+		
+		for (int i = 0; i < t; i++) {
+			int result = StdRandom.uniform(1, sides+1);
+			rolls[result].increment();
+		}
+		
+		for (int i = 1; i <= sides; i++)
+			StdOut.println(rolls[i]);
+	}
+}
