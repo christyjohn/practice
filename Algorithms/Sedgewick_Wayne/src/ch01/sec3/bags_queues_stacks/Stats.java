288a1fe30f97eea7d22a6686c03e4753b9ed866d
Christy John <christyjohn.crz@gmail.com>
2020-12-03 09:28:34 +0530

    Mean and Standard Deviance

---
+++ Algorithms/Sedgewick_Wayne/src/ch01/sec3/bags_queues_stacks/Stats.java
@@ -0,0 +1,28 @@
+package ch01.sec3.bags_queues_stacks;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Stats {
+	public static void main(String[] args) {
+		Bag<Double> numbers = new Bag<>();
+		
+		while (!StdIn.isEmpty())
+			numbers.add(StdIn.readDouble());
+		int n = numbers.size();
+		
+		double sum = 0.0;
+		for (double x : numbers)
+			sum += x;
+		double mean =sum / n;
+		
+		sum = 0.0;
+		for (double x : numbers)
+			sum += (x - mean) * (x - mean);
+		double std = Math.sqrt(sum/ (n-1));
+		
+		StdOut.printf("Mean: %.2f\n",  mean);;
+		StdOut.printf("Std dev: %.2f\n", std);
+	}
+}
