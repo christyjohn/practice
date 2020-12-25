4635c77ed064cc88d350bd363edb0ab19adcaba8
Christy John <christyjohn.crz@gmail.com>
2020-12-25 23:25:08 +0530

    Spring demo - Dependency Injection : Setter Injection

---
+++ Java/Spring/Udemy/Chad_Darby/01-spring-core-5/spring-setter-injection/src/com/christy/springdemo/TrackCoach.java
@@ -0,0 +1,21 @@
+package com.christy.springdemo;
+
+public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public TrackCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "Just Do It: " + fortuneService.getFortune();
+	}
+
+}
