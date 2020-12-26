769db553833b6ee67c7309c3360711013570d31b
Christy John <christyjohn.crz@gmail.com>
2020-12-26 22:57:37 +0530

    Dependency Injection using Annotations - Constructor injection

---
+++ Java/Spring/Udemy/Chad_Darby/01-spring-core-5/spring-annotation-constructor-injection/src/com/christy/springdemo/TennisCoach.java
@@ -0,0 +1,30 @@
+package com.christy.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public TennisCoach() { 
+		
+	}
+	
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
