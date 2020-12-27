29f6238b681d15750d703fe1f2bb29c48d081113
Christy John <christyjohn.crz@gmail.com>
2020-12-27 17:25:55 +0530

    Classes and Objects - part 1

---
+++ Java/Java_8/Kishori_Sharan/src/book1/ch06/classes_objects/Human.java
@@ -0,0 +1,7 @@
+package book1.ch06.classes_objects;
+
+public class Human {
+	String name;
+	String gender;
+	static long count;
+}
---
+++ Java/Java_8/Kishori_Sharan/src/book1/ch06/classes_objects/StaticImportTest.java
@@ -0,0 +1,9 @@
+package book1.ch06.classes_objects;
+
+import static java.lang.System.out;;
+
+public class StaticImportTest {
+	public static void main(String[] args) {
+		out.println("Hello static import!");
+	}
+}
