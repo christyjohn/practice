e52ddd453cf83b8dc016ae1d5438294a913002be
Christy John <christyjohn.crz@gmail.com>
2020-12-27 18:14:54 +0530

    Configuration using Java Config (no xml)

---
+++ Java/Spring/Udemy/Chad_Darby/01-spring-core-5/spring-java-config/src/com/christy/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.christy.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.christy.springdemo")
+public class SportConfig {
+
+}
