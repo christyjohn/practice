e5efb07f62a82aea5361af3370aa08bda356001c
Christy John <christyjohn.crz@gmail.com>
2021-01-31 04:42:28 +0530

    Developer skills - debugging

---
+++ JavaScript/core/Udemy/Jonas_Schmedtmann/01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,18 @@
+// values and variables
+const country = 'India';
+const continent = 'Asia';
+let population = '1.38 billion';
+
+console.log("Country: " + country);
+console.log("Continent: " + continent);
+console.log("Population: " + population);
+
+// data types
+let isIsland = false;
+let language;
+console.log(isIsland);
+console.log(language);
+
+language = 'Hindi';
+console.log(language);
+
---
+++ JavaScript/core/Udemy/Jonas_Schmedtmann/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,66 @@
+/*
+// Decalring variables
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+
+// Data types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Christy');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+console.log(typeof NaN);
+
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// Uncaught TypeError: Assignment to constant variable.
+//birthYear = 1990;
+
+// Uncaught SyntaxError: Missing initializer in const declaration
+//const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'John';
+console.log(lastName);
+*/
+
+// operators
+const now = 2037;
+const ageChristy = now - 1983;
+const ageSarah = now - 2018;
+console.log(ageChristy, ageSarah);
+
+console.log(ageChristy * 2, ageSarah / 10, 2 ** 3);
+
+const firstName = 'Christy';
+const lastName = 'John';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+console.log(x);
