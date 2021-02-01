97b1d2020f34a72078c6d514d926e493eccbe038
Christy John <christyjohn.crz@gmail.com>
2021-02-01 23:18:44 +0530

    Exception handling

---
+++ Java/Java_8/CompleteReference/src/ch10/exception_handling/Exc1.java
@@ -0,0 +1,12 @@
+package ch10.exception_handling;
+
+public class Exc1 {
+	static void subroutine() {
+		int d = 10;
+		int a = 10 / d;
+	}
+	
+	public static void main(String[] args) {
+		Exc1.subroutine();
+	}
+}
