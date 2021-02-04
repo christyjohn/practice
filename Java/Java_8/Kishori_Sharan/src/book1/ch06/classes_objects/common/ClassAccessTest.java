e2224c9f81cfc9c93d2dd76d09aa4abf2a102303
Christy John <christyjohn.crz@gmail.com>
2021-02-05 05:24:59 +0530

    Classes and objects

---
+++ Java/Java_8/Kishori_Sharan/src/book1/ch06/classes_objects/common/ClassAccessTest.java
@@ -0,0 +1,9 @@
+package book1.ch06.classes_objects.common;
+
+import book1.ch06.classes_objects.Human;
+
+public class ClassAccessTest {
+	public static void main(String[] args) {
+		Human jack;
+	}
+}
