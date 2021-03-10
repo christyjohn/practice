1f962fd6bfaf25c0b93dbe9bd7f5dde0990fc9e1
Christy John <christyjohn.crz@gmail.com>
2021-03-10 19:03:39 +0530

    Overview

---
+++ Java/Java_8/CompleteReference/src/ch02/overview/Example.java
@@ -0,0 +1,11 @@
+package ch02.overview;
+
+/*
+	This is a simple Java program.
+	Call this file "Example.java".
+*/
+public class Example {
+	public static void main(String[] args) {
+		System.out.println("This is a simple Java program.");
+	}
+}
