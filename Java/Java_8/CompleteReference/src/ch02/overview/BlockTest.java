b279f28d71f543b51a5de9d56746408e50c359fc
Christy John <christyjohn.crz@gmail.com>
2021-03-11 09:05:52 +0530

    Datatypes, Variables and arrays

--- Java/Java_8/CompleteReference/src/ch02/overview/BlockTest.java
+++ Java/Java_8/CompleteReference/src/ch02/overview/BlockTest.java
@@ -7,7 +7,7 @@ public class BlockTest {
 		y = 20;
 		
 		// the target of this loop is a block
-		for(x = 0; x<10; x++) {
+		for(x = 0; x < 10; x++) {
 			System.out.println("This is x: " + x);
 			System.out.println("This is y: " + y);
 			y = y - 2;
