8808c87a59970329612c42362c9cfe48f83abab8
Christy John <christyjohn.crz@gmail.com>
2021-04-20 17:08:11 +0530

    Classes, Constructors, and Inheritance

--- Java/Java_11/udemy_masterclass/src/sec06/oops1/Car.java
+++ Java/Java_11/udemy_masterclass/src/sec06/oops1/Car.java
@@ -1,8 +1,47 @@
 package sec06.oops1;
 
-public class Car {
+public class Car extends Vehicle {
     private int doors;
     private int wheels;
+    private String model;
     private String engine;
     private String colour;
+    private int gears;
+    private boolean isManual;
+    private int currentGear;
+
+    public Car() {
+        super();
+    }
+
+    public Car(String name, String size, int wheels, int doors, int gears, boolean isManual) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.currentGear = 1;
+    }
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore"))
+            this.model = model;
+        else
+            this.model = "Unknown";
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void changeGear(int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Change to " + this.currentGear + " gear.");
+    }
+
+    public void changeVelocity(int speed, int direction) {
+        move(speed,direction);
+        System.out.println("Car.changeVelocity() : Velocity " + speed + " direction " + direction);
+    }
 }
--- Java/Java_11/udemy_masterclass/src/sec06/oops1/CarApp.java
+++ Java/Java_11/udemy_masterclass/src/sec06/oops1/CarApp.java
@@ -4,5 +4,15 @@ public class CarApp {
     public static void main(String[] args) {
         Car porsche = new Car();
         Car holden = new Car();
+
+        porsche.setModel("Carrera");
+
+        System.out.println("Model is " + porsche.getModel());
+
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
     }
 }
