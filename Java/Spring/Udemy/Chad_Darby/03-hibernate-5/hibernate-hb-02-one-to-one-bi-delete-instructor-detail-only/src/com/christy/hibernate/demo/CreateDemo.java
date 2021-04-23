3f0aeceb89f80592f8e0a3b7be8121e01d8a202a
Christy John <christyjohn.crz@gmail.com>
2021-04-23 11:45:13 +0530

    One to One Mapping - Bidirectional - Delete only Instructor Detail not Instructor

---
+++ Java/Spring/Udemy/Chad_Darby/03-hibernate-5/hibernate-hb-02-one-to-one-bi-delete-instructor-detail-only/src/com/christy/hibernate/demo/CreateDemo.java
@@ -0,0 +1,56 @@
+package com.christy.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.christy.hibernate.demo.entity.Instructor;
+import com.christy.hibernate.demo.entity.InstructorDetail;
+
+public class CreateDemo {
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+				
+		// create session
+		Session session = factory.getCurrentSession();
+		
+try {			
+			
+			// create the objects
+			Instructor tempInstructor1 = 
+					new Instructor("Chris", "Martin", "Chris Martin@gmail.com");
+			
+			InstructorDetail tempInstructorDetail1 =
+					new InstructorDetail(
+							"http://www.martin-chris.com",
+							"Movies");		
+			
+			// associate the objects
+			tempInstructor1.setInstructorDetail(tempInstructorDetail1);
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the instructor
+			//
+			// Note: this will ALSO save the details object
+			// because of CascadeType.ALL
+			//
+			System.out.println("Saving instructor: " + tempInstructor1);
+			session.save(tempInstructor1);								
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+}
