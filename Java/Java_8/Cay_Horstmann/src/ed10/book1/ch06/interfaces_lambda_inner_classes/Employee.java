5bfa89abdddc5b7d8ee8bed2c34c33c258f01f10
Christy John <christyjohn.crz@gmail.com>
2021-05-01 16:03:18 +0530

    Callback and cloning

--- Java/Java_8/Cay_Horstmann/src/ed10/book1/ch06/interfaces_lambda_inner_classes/Employee.java
+++ Java/Java_8/Cay_Horstmann/src/ed10/book1/ch06/interfaces_lambda_inner_classes/Employee.java
@@ -1,12 +1,17 @@
 package ed10.book1.ch06.interfaces_lambda_inner_classes;
 
-public class Employee implements Comparable<Employee> {
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Employee implements Comparable<Employee>, Cloneable {
 	private String name;
 	private double salary;
+	private Date hireDay;
 
 	public Employee(String name, double salary) {
 		this.name = name;
 		this.salary = salary;
+	      hireDay = new Date();
 	}
 
 	public String getName() {
@@ -17,9 +22,33 @@ public class Employee implements Comparable<Employee> {
 		return salary;
 	}
 
+	/**
+	 * Set the hire day to a given date.
+	 * 
+	 * @param year  the year of the hire day
+	 * @param month the month of the hire day
+	 * @param day   the day of the hire day
+	 */
+	public void setHireDay(int year, int month, int day) {
+		Date newHireDay = new GregorianCalendar(year, month - 1, day).getTime();
+
+		// Example of instance field mutation
+		hireDay.setTime(newHireDay.getTime());
+	}
+
 	public void raiseSalary(double byPercent) {
 		double raise = salary * byPercent / 100;
 		salary += raise;
+	}
+	
+	public Employee clone() throws CloneNotSupportedException {
+		// call object.clone()
+		Employee cloned = (Employee) super.clone();
+		
+		// clone mutable fields
+		cloned.hireDay = (Date) hireDay.clone();
+		
+		return cloned;
 	}
 
 	/**
@@ -32,5 +61,9 @@ public class Employee implements Comparable<Employee> {
 	 */
 	public int compareTo(Employee other) {
 		return Double.compare(salary, other.salary);
+	}
+
+	public String toString() {
+		return "Employee[name=" + name + ",salary=" + salary + ",hireDay=" + hireDay + "]";
 	}
 }
