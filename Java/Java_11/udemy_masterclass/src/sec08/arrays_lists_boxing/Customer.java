f81ffdb83d21bdf3421860253fc2ced3f86912fb
Christy John <christyjohn.crz@gmail.com>
2021-05-04 17:39:33 +0530

    LinkedList example

--- Java/Java_11/udemy_masterclass/src/sec08/arrays_lists_boxing/Customer.java
+++ Java/Java_11/udemy_masterclass/src/sec08/arrays_lists_boxing/Customer.java
@@ -12,15 +12,16 @@ public class Customer {
         addTransaction(initialAmount);
     }
 
-    public void addTransaction(double amount) {
-        this.transactions.add(amount);
-    }
-
     public String getName() {
         return name;
+    }
+
+    public void addTransaction(double amount) {
+        this.transactions.add(amount);
     }
 
     public ArrayList<Double> getTransactions() {
         return transactions;
     }
+
 }
