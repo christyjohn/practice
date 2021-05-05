e0808bca78035354066a6a79e73b67e2fcede22d
Christy John <christyjohn.crz@gmail.com>
2021-05-05 10:25:25 +0530

    Abstract class demo

---
+++ Java/Java_11/udemy_masterclass/src/sec09/innerclass_abstractclass_interfaces/CanFly.java
@@ -0,0 +1,5 @@
+package sec09.innerclass_abstractclass_interfaces;
+
+public interface CanFly {
+    void fly();
+}
---
+++ Java/Java_11/udemy_masterclass/src/sec09/innerclass_abstractclass_interfaces/Parrot.java
@@ -0,0 +1,9 @@
+package sec09.innerclass_abstractclass_interfaces;
+
+public class Parrot extends Bird {
+
+    public Parrot(String name) {
+        super(name);
+    }
+
+}
