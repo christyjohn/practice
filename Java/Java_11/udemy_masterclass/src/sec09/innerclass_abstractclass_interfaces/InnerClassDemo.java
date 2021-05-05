ad0979f9a56c78accfd07a8f47cf83a2c5105e83
Christy John <christyjohn.crz@gmail.com>
2021-05-05 09:31:18 +0530

    Inner class demo

---
+++ Java/Java_11/udemy_masterclass/src/sec09/innerclass_abstractclass_interfaces/InnerClassDemo.java
@@ -0,0 +1,20 @@
+package sec09.innerclass_abstractclass_interfaces;
+
+public class InnerClassDemo {
+    public static void main(String[] args) {
+        GearBox mcLaren = new GearBox(6);
+        mcLaren.addGear(1, 5.3);
+        mcLaren.addGear(2, 10.6);
+        mcLaren.addGear(3, 15.9);
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(2);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
+    }
+}
