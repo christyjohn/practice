a9173fdf559747aeebafee4fb5e1992678a15b5e
Christy John <christyjohn.crz@gmail.com>
2021-05-06 10:56:47 +0530

    Creating a RESTful Web Service with Spring Boot Actuator

---
+++ Java/Spring/spring_io/guides/REST/actuator-service/src/test/java/com/christy/actuatorservice/ActuatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christy.actuatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
