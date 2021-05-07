5c23f9abefd2a82625361cf8db87eb50e5052a5f
Christy John <christyjohn.crz@gmail.com>
2021-05-07 21:12:02 +0530

    Immutable class example

--- Java/Java_11/udemy_masterclass/src/sec12/collections/Location.java
+++ Java/Java_11/udemy_masterclass/src/sec12/collections/Location.java
@@ -8,10 +8,10 @@ public class Location {
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<>();
+        this.exits = new HashMap<>(exits);
         this.exits.put("Q", 0);
     }
 
