6eed66f494de81a35cdf36d5d2a1bf765cc03065
Christy John <christyjohn.crz@gmail.com>
2021-05-08 08:45:57 +0530

    equals and hashCode

--- Java/Java_11/udemy_masterclass/src/sec12/collections/HeavenlyBody.java
+++ Java/Java_11/udemy_masterclass/src/sec12/collections/HeavenlyBody.java
@@ -3,7 +3,7 @@ package sec12.collections;
 import java.util.HashSet;
 import java.util.Set;
 
-public class HeavenlyBody {
+public final class HeavenlyBody {
     private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
@@ -28,5 +28,27 @@ public class HeavenlyBody {
 
     public Set<HeavenlyBody> getSatellites() {
         return new HashSet<>(this.satellites);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        System.out.println("the obj.getClass() is " + obj.getClass());
+        System.out.println("this.getClass() is " + this.getClass());
+        if ((obj == null || (obj.getClass() != this.getClass()))) {
+            return false;
+        }
+
+        String objName = ((HeavenlyBody) obj).getName();
+        return this.name.equals(objName);
+    }
+
+    @Override
+    public int hashCode() {
+        System.out.println("Hashcode called");
+        return this.name.hashCode() + 57;
     }
 }
