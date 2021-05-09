fa543abc65ed8fc5adf024f1c31dbdea63c22343
Christy John <christyjohn.crz@gmail.com>
2021-05-09 11:54:56 +0530

    Sets Challenge

---
+++ Java/Java_11/udemy_masterclass/src/sec12/collections/Moon.java
@@ -0,0 +1,7 @@
+package sec12.collections;
+
+public class Moon extends HeavenlyBody {
+    public Moon(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.MOON);
+    }
+}
