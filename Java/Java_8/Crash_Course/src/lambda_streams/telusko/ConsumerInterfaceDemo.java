740dda1e82f9e24937055c6a5e361c5f7133a8a6
Christy John <christyjohn.crz@gmail.com>
2021-06-11 14:54:06 +0530

    Use lambda expression on Consumer interface

--- Java/Java_8/Crash_Course/src/lambda_streams/telusko/ConsumerInterfaceDemo.java
+++ Java/Java_8/Crash_Course/src/lambda_streams/telusko/ConsumerInterfaceDemo.java
@@ -9,12 +9,8 @@ public class ConsumerInterfaceDemo {
 	public static void main(String[] args) {
 		List<Integer> values = Arrays.asList(1,2,3, 4,5,6,7);
 		
-		Consumer<Integer> c = new Consumer<Integer>() {
-			public void accept(Integer i) {
-				System.out.println(i);
-			}
-		};
+		//Consumer<Integer> c = i -> System.out.println(i);
 		
-		values.forEach(c);
+		values.forEach(i -> System.out.println(i));
 	}
 }
