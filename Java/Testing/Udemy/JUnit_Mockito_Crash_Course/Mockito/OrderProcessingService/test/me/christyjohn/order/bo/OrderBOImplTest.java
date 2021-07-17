5a6b37381e9224ab7ae05476446d94f1348bcb57
Christy John <christyjohn.crz@gmail.com>
2021-07-17 20:41:21 +0530

    Test cancel order

--- Java/Testing/Udemy/JUnit_Mockito_Crash_Course/Mockito/OrderProcessingService/test/me/christyjohn/order/bo/OrderBOImplTest.java
+++ Java/Testing/Udemy/JUnit_Mockito_Crash_Course/Mockito/OrderProcessingService/test/me/christyjohn/order/bo/OrderBOImplTest.java
@@ -17,38 +17,58 @@ import me.christyjohn.order.dao.OrderDAO;
 import me.christyjohn.order.dto.Order;
 
 public class OrderBOImplTest {
-	
+
 	@Mock
 	OrderDAO dao;
 	private OrderBOImpl bo;
-	
+
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
-		
+
 		bo = new OrderBOImpl();
-		bo.setDao(dao);		
+		bo.setDao(dao);
 	}
 
 	@Test
 	public void placeOrder_Should_Create_An_Order() throws SQLException, BOException {
-		
+
 		Order order = new Order();
 		when(dao.create(order)).thenReturn(new Integer(1));
 		boolean result = bo.placeOrder(order);
-		
+
 		assertTrue(result);
 		verify(dao).create(order);
 	}
 
 	@Test
 	public void placeOrder_Should_Not_Create_An_Order() throws SQLException, BOException {
-				
+
 		Order order = new Order();
 		when(dao.create(order)).thenReturn(new Integer(0));
 		boolean result = bo.placeOrder(order);
-		
+
 		assertFalse(result);
 		verify(dao).create(order);
+	}
+
+	@Test(expected = BOException.class)
+	public void placeOrder_Should_Throw_BOException() throws SQLException, BOException {
+
+		Order order = new Order();
+		when(dao.create(order)).thenThrow(SQLException.class);
+		bo.placeOrder(order);
+	}
+	
+	@Test
+	public void cancelOrder_Should_Cancel_The_Order() throws SQLException, BOException {
+		Order order = new Order();
+		when(dao.read(123)).thenReturn(order);
+		when(dao.update(order)).thenReturn(1);
+		boolean result = bo.cancelOrder(123);
+		
+		assertTrue(result);
+		verify(dao).read(123);
+		verify(dao).update(order);
 	}
 }
