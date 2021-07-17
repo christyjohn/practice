3ad936cdc27a98b7e0ad58c762bad3f4940aeb51
Christy John <christyjohn.crz@gmail.com>
2021-07-17 10:39:04 +0530

    First JUnit 4 test

---
+++ Java/Testing/JUnit_Mockito_Crash_Course/src/me/christyjohn/tuts/junit/Greeting.java
@@ -0,0 +1,5 @@
+package me.christyjohn.tuts.junit;
+
+public interface Greeting {
+	String greet(String name);
+}
---
+++ Java/Testing/JUnit_Mockito_Crash_Course/src/me/christyjohn/tuts/junit/GreetingImpl.java
@@ -0,0 +1,10 @@
+package me.christyjohn.tuts.junit;
+
+public class GreetingImpl implements Greeting {
+
+	@Override
+	public String greet(String name) {
+		return "Hello " + name;
+	}
+
+}
