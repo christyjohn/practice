84c716f3f31491a8955bb2123971c96b7aea8813
Christy John <christyjohn.crz@gmail.com>
2021-07-18 10:48:18 +0530

    Mock static methods and test

--- Java/Testing/Udemy/JUnit_Mockito_Crash_Course/PowerMock/UserAdminService/test/me/christyjohn/useradmin/dao/UserDAOTest.java
+++ Java/Testing/Udemy/JUnit_Mockito_Crash_Course/PowerMock/UserAdminService/test/me/christyjohn/useradmin/dao/UserDAOTest.java
@@ -1,18 +1,33 @@
 package me.christyjohn.useradmin.dao;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.*;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import me.christyjohn.useradmin.dto.User;
+import me.christyjohn.useradmin.util.IDGenerator;
+
+import static org.powermock.api.mockito.PowerMockito.*;
+
 @RunWith(PowerMockRunner.class)
+@PrepareForTest(IDGenerator.class)
 public class UserDAOTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void createShouldReturnAUserId() {
+		UserDAO dao = new UserDAO();
+		
+		mockStatic(IDGenerator.class);
+		when(IDGenerator.generateID()).thenReturn(1);
+		
+		int result = dao.create(new User());
+		assertEquals(1, result);
+		verifyStatic();
 	}
 
 }
