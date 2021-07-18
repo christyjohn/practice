dca70eb88f577c4d52d56c379f6804768072ffa4
Christy John <christyjohn.crz@gmail.com>
2021-07-18 20:14:14 +0530

    Test create method

--- Java/Testing/Udemy/JUnit_Mockito_Crash_Course/SpringBootMicroService/couponservice/src/test/java/me/christyjohn/springcloud/CouponserviceApplicationTests.java
+++ Java/Testing/Udemy/JUnit_Mockito_Crash_Course/SpringBootMicroService/couponservice/src/test/java/me/christyjohn/springcloud/CouponserviceApplicationTests.java
@@ -1,13 +1,39 @@
 package me.christyjohn.springcloud;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import me.christyjohn.springcloud.controllers.CouponRestController;
+import me.christyjohn.springcloud.model.Coupon;
+import me.christyjohn.springcloud.repos.CouponRepo;
+
 @SpringBootTest
-class CouponserviceApplicationTests {
+public class CouponserviceApplicationTests {
+	
+	private static final String SUPERSALE = "SUPERSALE";
+
+	@Mock
+	private CouponRepo repo;
+
+	@InjectMocks
+	private CouponRestController controller;
 
 	@Test
-	void contextLoads() {
+	public void testCreate() {
+		Coupon coupon =new Coupon();
+		coupon.setCode(SUPERSALE);
+		when(repo.save(coupon)).thenReturn(coupon);
+		Coupon couponCreated = controller.create(coupon);
+		verify(repo).save(coupon);
+		assertNotNull(couponCreated);
+		assertEquals(SUPERSALE,coupon.getCode());
 	}
 
 }
