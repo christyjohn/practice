c5310655a5a4b3d385e3412dab8a63d63f8c0cae
Christy John <christyjohn.crz@gmail.com>
2021-07-18 10:40:34 +0530

    Converted to Maven project

---
+++ Java/Testing/Udemy/JUnit_Mockito_Crash_Course/PowerMock/UserAdminService/test/me/christyjohn/useradmin/dao/UserDAOTest.java
@@ -0,0 +1,18 @@
+package me.christyjohn.useradmin.dao;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.*;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+public class UserDAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
