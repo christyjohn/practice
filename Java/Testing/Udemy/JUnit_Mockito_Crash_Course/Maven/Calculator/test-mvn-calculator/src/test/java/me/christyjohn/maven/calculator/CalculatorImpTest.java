fd3137060a4b611c94954f18199ecd180f004dfa
Christy John <christyjohn.crz@gmail.com>
2021-07-18 09:31:10 +0530

    create parameterized test

--- Java/Testing/Udemy/JUnit_Mockito_Crash_Course/Maven/Calculator/test-mvn-calculator/src/test/java/me/christyjohn/maven/calculator/CalculatorImpTest.java
+++ Java/Testing/Udemy/JUnit_Mockito_Crash_Course/Maven/Calculator/test-mvn-calculator/src/test/java/me/christyjohn/maven/calculator/CalculatorImpTest.java
@@ -6,21 +6,34 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class CalculatorImpTest {
 	
 	private int num1;
 	private int num2;
-	private int result;
+	private int expectedResult;
 	
 	public CalculatorImpTest(int num1, int num2, int result) {
 		this.num1 = num1;
 		this.num2 = num2;
-		this.result = result;
+		this.expectedResult = result;
 	}
 	
+	// parameterized tests
+	@Parameters
 	public static Collection<Integer[]> data() {
 		return Arrays.asList(new Integer[][] { {-1, 2, 1}, {1, 2, 3}, {6, 7, 13} });
+	}
+	
+	@Test
+	public void addShouldReturnAResult_Parameterized() {
+		Calculator c = new CalculatorImp();
+		int result = c.add(num1, num2);
+		assertEquals(expectedResult, result);
 	}
 
 	@Test
