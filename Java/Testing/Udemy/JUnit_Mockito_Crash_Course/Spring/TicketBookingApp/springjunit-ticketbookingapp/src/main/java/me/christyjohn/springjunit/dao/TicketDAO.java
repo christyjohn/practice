d463142831504c02a4e7332cac53ca13f8741507
Christy John <christyjohn.crz@gmail.com>
2021-07-18 12:27:43 +0530

    Create DAO class

---
+++ Java/Testing/Udemy/JUnit_Mockito_Crash_Course/Spring/TicketBookingApp/springjunit-ticketbookingapp/src/main/java/me/christyjohn/springjunit/dao/TicketDAO.java
@@ -0,0 +1,7 @@
+package me.christyjohn.springjunit.dao;
+
+import me.christyjohn.springjunit.dto.Ticket;
+
+public interface TicketDAO {
+	int createTicket(Ticket ticket);
+}
---
+++ Java/Testing/Udemy/JUnit_Mockito_Crash_Course/Spring/TicketBookingApp/springjunit-ticketbookingapp/src/main/java/me/christyjohn/springjunit/dao/TicketDAOImpl.java
@@ -0,0 +1,12 @@
+package me.christyjohn.springjunit.dao;
+
+import me.christyjohn.springjunit.dto.Ticket;
+
+public class TicketDAOImpl implements TicketDAO {
+
+	@Override
+	public int createTicket(Ticket ticket) {
+		return 1;
+	}
+
+}
