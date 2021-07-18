842f7376a7000a20653eeb078b86974b87a315d1
Christy John <christyjohn.crz@gmail.com>
2021-07-18 15:57:37 +0530

    Create Model and Repository

---
+++ Java/Testing/Udemy/JUnit_Mockito_Crash_Course/SpringBootMicroService/couponservice/src/main/java/me/christyjohn/springcloud/repos/CouponRepo.java
@@ -0,0 +1,9 @@
+package me.christyjohn.springcloud.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import me.christyjohn.springcloud.model.Coupon;
+
+public interface CouponRepo extends JpaRepository<Coupon, Long> {
+
+}
