d8f4533c8689bf2ed493a001ffecd82bdc8f06a0
Christy John <christyjohn.crz@gmail.com>
2021-08-07 14:59:59 +0530

    Crreated Rest Cotroller and print all the Beans initialized

--- Java/Spring/Udemy/In28Minutes/Master_MicroServices/springboot-in-10-steps/src/main/java/me/christyjohn/springboot/basics/SpringbootIn10StepsApplication.java
+++ Java/Spring/Udemy/In28Minutes/Master_MicroServices/springboot-in-10-steps/src/main/java/me/christyjohn/springboot/basics/SpringbootIn10StepsApplication.java
@@ -1,13 +1,18 @@
-package me.christyjohn.springboot.basics;
+ package me.christyjohn.springboot.basics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
