bebb7b96d48d41cb95f16361f04b083611b36508
Christy John <christyjohn.crz@gmail.com>
2021-08-12 18:14:53 +0530

    Added Null Check

--- Java/Testing/PracticalUnitTesting_ed1/src/main/java/com/practicalunittesting/chp03/exercises/StringReverseApp.java
+++ Java/Testing/PracticalUnitTesting_ed1/src/main/java/com/practicalunittesting/chp03/exercises/StringReverseApp.java
@@ -6,18 +6,18 @@ import java.util.List;
 public class StringReverseApp {
 
 	public static String reverse(String s) {
-		if (s.length() < 2) {
-			throw new IllegalArgumentException("String length less than 2: [" + s + "]");
+		if (s.equals(null)) {
+			throw new NullPointerException("Null String");
 		}
 		
-		List<String> tempArray = new ArrayList<>(s.length());
+		List<String> tempArray = new ArrayList<String>(s.length());
 		for (int i = 0; i < s.length(); i++) {
 			tempArray.add(s.substring(i, i+1));
 		}
 		StringBuilder reversedString = new StringBuilder(s.length());
-		for(int i = tempArray.size()-1; i >=0; i--) {
+		for (int i = tempArray.size() -1; i >= 0; i--) {
 			reversedString.append(tempArray.get(i));
 		}
-		return reversedString.toString();		
+		return reversedString.toString();
 	}
 }
