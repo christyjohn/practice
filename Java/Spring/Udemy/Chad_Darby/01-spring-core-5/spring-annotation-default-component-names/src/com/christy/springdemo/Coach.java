0bd9f66a052558780515a3e7552e41d22f5d7a24
Christy John <christyjohn.crz@gmail.com>
2021-12-15 16:28:22 +0530

    Clean up

--- Java/Spring/Udemy/Chad_Darby/01-spring-core-5/spring-annotation-default-component-names/src/com/christy/springdemo/Coach.java
+++
@@ -1,7 +0,0 @@
-package com.christy.springdemo;
-
-public interface Coach {
-	
-	public String getDailyWorkout();
-	
-}
--- Java/Spring/Udemy/Chad_Darby/01-spring-core-5/spring-annotation-field-injection/src/com/christy/springdemo/Coach.java
+++
@@ -1,9 +0,0 @@
-package com.christy.springdemo;
-
-public interface Coach {
-	
-	public String getDailyWorkout();
-	
-	public String getDailyFortune();
-	
-}
