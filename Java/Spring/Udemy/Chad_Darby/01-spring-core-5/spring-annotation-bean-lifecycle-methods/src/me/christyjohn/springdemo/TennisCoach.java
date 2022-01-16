a8a02a6a4029aa8931eb63abef03fb292efcd1ad
Christy John <christyjohn.crz@gmail.com>
2022-01-16 18:15:03 +0530

    Spring annotations: Bean Lifecycle methods

---
+++ Java/Spring/Udemy/Chad_Darby/01-spring-core-5/spring-annotation-bean-lifecycle-methods/src/me/christyjohn/springdemo/TennisCoach.java
@@ -0,0 +1,43 @@
+package me.christyjohn.springdemo;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+	
+	@Autowired
+	@Qualifier("happyFortuneService")
+	private FortuneService fortuneService;
+	
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff");
+	}
+	
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff");
+	}
+		
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
