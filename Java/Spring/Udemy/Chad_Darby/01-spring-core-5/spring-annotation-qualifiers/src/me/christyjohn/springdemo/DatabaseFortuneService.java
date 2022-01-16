dbe470fdfb5299d389193432f94595fed3be73cb
Christy John <christyjohn.crz@gmail.com>
2022-01-16 17:05:40 +0530

    Spring annotations: Qualifiers

---
+++ Java/Spring/Udemy/Chad_Darby/01-spring-core-5/spring-annotation-qualifiers/src/me/christyjohn/springdemo/DatabaseFortuneService.java
@@ -0,0 +1,13 @@
+package me.christyjohn.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return null;
+	}
+
+}
