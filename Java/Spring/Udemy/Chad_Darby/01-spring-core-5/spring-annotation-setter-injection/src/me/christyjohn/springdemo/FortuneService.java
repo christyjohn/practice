d0868cbe4f28b86c65690f7bcd4fa06cc9f33c22
Christy John <christyjohn.crz@gmail.com>
2022-01-16 15:54:05 +0530

    Spring annotations: Setter Injection

---
+++ Java/Spring/Udemy/Chad_Darby/01-spring-core-5/spring-annotation-setter-injection/src/me/christyjohn/springdemo/FortuneService.java
@@ -0,0 +1,7 @@
+package me.christyjohn.springdemo;
+
+public interface FortuneService {
+
+	public String getFortune();
+	
+}
---
+++ Java/Spring/Udemy/Chad_Darby/01-spring-core-5/spring-annotation-setter-injection/src/me/christyjohn/springdemo/TennisCoach.java
@@ -0,0 +1,32 @@
+package me.christyjohn.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	// define setter method
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside setFortuneService");
+		fortuneService = theFortuneService;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
