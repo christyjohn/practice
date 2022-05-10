5b5ee5941a72d5876fddfb98728f5f4d70a94e8f
Christy John <christyjohn.crz@gmail.com>
2022-05-11 05:05:27 +0530

    cleaning up

--- Java/Testing/Udemy/JUnit_Mockito_Crash_Course/SpringBootMicroService/productservice/src/main/java/me/christyjohn/springcloud/repos/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package me.christyjohn.springcloud.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import me.christyjohn.springcloud.model.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {
-
-}
