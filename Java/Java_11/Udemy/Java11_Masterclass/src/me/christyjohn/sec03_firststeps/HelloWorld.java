652353a82e34479108252abebe945cdb0223faa5
Christy John <christyjohn.crz@gmail.com>
2022-05-11 18:36:54 +0530

    cleaning up

--- Java/Java_11/Udemy/Java11_Masterclass/src/me/christyjohn/sec03_firststeps/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package me.christyjohn.sec03_firststeps;
-
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-
-}
--- Java/Yorbit/101_Java_Tutorial_for_Complete_Beginners/src/com/christy/sec02/core/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.christy.sec02.core;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
-
-}
