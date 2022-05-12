960013ce27561e63e4a18c5c6dc0d7bfe83bc155
Christy John <christyjohn.crz@gmail.com>
2022-05-12 10:58:58 +0530

    A real world BiConsumer example

--- Java/Java 8/Udemy/ModernJava/src/me/christyjohn/functionalInterfaces/BiConsumerExample.java
+++ Java/Java 8/Udemy/ModernJava/src/me/christyjohn/functionalInterfaces/BiConsumerExample.java
@@ -1,11 +1,23 @@
 package me.christyjohn.functionalInterfaces;
 
+import me.christyjohn.data.Student;
+import me.christyjohn.data.StudentDataBase;
+
+import java.util.List;
 import java.util.function.BiConsumer;
 
 public class BiConsumerExample {
 
+    static List<Student> studentList = StudentDataBase.getAllStudents();
+    static BiConsumer<String, List<String>> biConsumer = (name, activities) ->
+            System.out.println(name + " : " + activities);
+
+    public static void printNameAndActivites() {
+        studentList.forEach(student -> biConsumer.accept(student.getName(), student.getActivities()));
+    }
+
     public static void main(String[] args) {
-        BiConsumer<String, String> biConsumer = (a, b) -> System.out.println(a + " " + b);
+        /*BiConsumer<String, String> biConsumer = (a, b) -> System.out.println(a + " " + b);
         biConsumer.accept("Christy", "John");
 
         BiConsumer<Integer, Integer> multiply = (a, b) -> System.out.println("Multiplication : " +(a * b));
@@ -13,6 +25,8 @@ public class BiConsumerExample {
 
         //Multiplication : 50
         //Division : 2
-        multiply.andThen(division).accept(10, 5);
+        multiply.andThen(division).accept(10, 5);*/
+
+        printNameAndActivites();
     }
 }
